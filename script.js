ea49535dd67fcfccb379f7736bdb5f4b578ecd78
NiklasDamhofer <niklas.damhofer@gmx.at>
2023-10-21 14:41:21 +0200

    debug html

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let display = document.getElementById('display');
+let display = document.querySelector('#display');
 
+let packet1 = "";
+
+let packet2 = "";
 
 function showInput() {
-    const content = document.createElement('h3');
+    const content = document.createElement('div');
     content.classList.add('input');
-    
+    content.textContent = "13";
+    display.append(content);
 }
+
+showInput();
