35c8e128441eb1383010e86153cf62a6c4ecec4e
NiklasDamhofer <niklas.damhofer@gmx.at>
2023-10-22 22:08:05 +0200

    btn

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ let packet1 = "";
 
 let packet2 = "";
 
+const btn7 = document.getElementById('btn7');
+const btn8 = document.getElementById('btn8');
+const btn9 = document.getElementById('btn9');
+const divbtn = document.getElementById('divbtn');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+const btnx = document.getElementById('btnx');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const minbtn = document.getElementById('minbtn');
+const combtn = document.getElementById('combtn');
+const btn0 = document.getElementById('btn0');
+const btnequ = document.getElementById('btnequ');
+const btnadd = document.getElementById('btnadd');
+
+
+
+
 function showInput() {
     const content = document.createElement('div');
     content.classList.add('input');
@@ -11,4 +31,7 @@ function showInput() {
     display.append(content);
 }
 
+
+
 showInput();
+
